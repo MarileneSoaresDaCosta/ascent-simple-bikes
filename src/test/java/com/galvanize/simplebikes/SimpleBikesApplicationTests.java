40decf1fa253b639ee24d88dff5dd6924a2b7f4a
Marilene Soares da Costa <lene626@gmail.com>
2022-08-05 14:15:22 -0600

    passes POST integration test

--- src/test/java/com/galvanize/simplebikes/SimpleBikesApplicationTests.java
+++ src/test/java/com/galvanize/simplebikes/SimpleBikesApplicationTests.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -76,5 +75,20 @@ class SimpleBikesApplicationTests {
 
     // add bike
 
+
+    @Test
+    void addBike_valid_returnsNewBike() {
+        Bike bike  = new Bike("Wing", 40, "RED");
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", MediaType.APPLICATION_JSON_VALUE);
+        HttpEntity<Bike> request = new HttpEntity<>(bike, headers);
+
+        // Act
+        ResponseEntity<Bike> response = restTemplate.postForEntity("/api/bikes", request, Bike.class);
+
+        // Assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody().getModel()).isEqualTo(bike.getModel());
+    }
 
 }
