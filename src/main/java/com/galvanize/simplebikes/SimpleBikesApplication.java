511135ebbae2c2848f0b93444061a955111d32bd
Marilene Soares da Costa <lene626@gmail.com>
2022-08-05 09:30:40 -0600

    Initial commit

---
+++ src/main/java/com/galvanize/simplebikes/SimpleBikesApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.simplebikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBikesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/simplebikes/SimpleBikesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.simplebikes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBikesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
